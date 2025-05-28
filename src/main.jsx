a8e8995511daf2da915d366bf5748eb0e7b5dad6
oguchirui <oguchi.rui.t8@dc.tohoku.ac.jp>
2025-05-28 21:39:07 +0900

    refactor: vercel Speed Insightsに対応

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
 import { createRoot } from 'react-dom/client'
 import './styles/index.css'
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  // <StrictMode>
+  <>
+    {/* <StrictMode> */}
+      <SpeedInsights />
       <App />
-  // </StrictMode>
-)
+    {/* </StrictMode> */}
+  </>
+);
