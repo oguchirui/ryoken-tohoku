d4a7df3988fd3865f7e612d541e10af992958062
oguchirui <oguchi.rui.t8@dc.tohoku.ac.jp>
2025-05-29 23:34:42 +0900

    feat: Aboutページを開設

--- src/components/AboutPage.jsx
+++ src/components/AboutPage.jsx
@@ -1,10 +1,17 @@
-
+import TableOfContents from "./TableOfContents";
+import AboutContent from "./AboutContent";
 
 const AboutPage = () => {
-
+  const sections = [
+    {id: 'introduction', title: 'はじめに' },
+    { id: 'howtouse', title: '使い方' },
+    { id: 'password', title: 'パスワードを忘れたときは？' },
+  ]
   return (
-    <>
-    </>
+    <div className="about-page">
+      <TableOfContents sections={sections}/>
+      <AboutContent sections={sections}/>
+    </div>
   );
 };
 
